300e32f8fe014f16cac7409f5a17c279ada17dcd
Erlend Eriksen <erlend_ne@hotmail.com>
2017-03-07 02:28:35 +0100

    minor innecessities cleanup

--- scripts/registerRosMsgSrv.sh
+++ scripts/registerRosMsgSrv.sh
@@ -2,5 +2,4 @@
 # NEEDS TO BE SOURCED: source <path-to-script>
 
 ROOT_DIR="$(realpath $(dirname ${BASH_SOURCE[0]}))/.."
-export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$ROOT_DIR
 export PYTHONPATH=$PYTHONPATH:$ROOT_DIR/build-native
